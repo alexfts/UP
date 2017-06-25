51b61b3e91b2d164fbd9b8f160b769d06898db20
Reza Asad <reza@Rezas-MacBook-Pro.local>
2017-06-25 13:59:10 -0700

    The scene now has infinite background and balloon going up

--- Up/GameScene.swift
+++ Up/GameScene.swift
@@ -14,8 +14,10 @@ class GameScene: SKScene {
     private var bg1: BGClass?
     private var bg2: BGClass?
     private var bg3: BGClass?
-    
     private var mainCamera: SKCameraNode?
+    private var balloon: BalloonClass?
+    
+    let UPWARD_SPEED: CGFloat = 5
     
     override func didMove(to view: SKView)
     {
@@ -24,23 +26,37 @@ class GameScene: SKScene {
     
     override func update(_ currentTime: TimeInterval)
     {
+        manageBG()
         manageCamera()
+        manageBalloon()
     }
     
     private func initializeGame()
     {
         mainCamera = childNode(withName: "MainCamera") as? SKCameraNode
-        bg1 = childNode(withName: "BG1") as? BGClass
-        bg2 = childNode(withName: "BG2") as? BGClass
-        bg3 = childNode(withName: "BG3") as? BGClass
+        bg1 = childNode(withName: "BG1") as? BGClass!
+        bg2 = childNode(withName: "BG2") as? BGClass!
+        bg3 = childNode(withName: "BG3") as? BGClass!
+        balloon = childNode(withName: "balloon") as? BalloonClass
+        balloon!.wiggleBalloon()
         
     }
     
     private func manageCamera()
     {
-        self.mainCamera!.position.y += 5;
+        self.mainCamera?.position.y += UPWARD_SPEED
     }
     
-    // Function to add the ballon moving up and wiggling
-    var baloon = 0
+    private func manageBalloon()
+    {
+        self.balloon?.position.y += UPWARD_SPEED
+    }
+    
+    private func manageBG()
+    {
+        bg1?.moveBG(camera: mainCamera!)
+        bg2?.moveBG(camera: mainCamera!)
+        bg3?.moveBG(camera: mainCamera!)
+    }
+    
 } // class
