58b8074db95d146fc9eb75b809e5b62ea1192244
Reza Asad <reza@Rezas-MacBook-Pro.local>
2017-06-25 13:57:38 -0700

    Balloon class created and moves with the camera

--- Up/BalloonClass.swift
+++ Up/BalloonClass.swift
@@ -6,4 +6,26 @@
 //  Copyright © 2017 Alex Fetisova. All rights reserved.
 //
 
-import Foundation
+import SpriteKit
+
+class BalloonClass: SKSpriteNode
+{
+    private var balloonAnimation  = [SKTexture]()
+    private var animateBalloonWiggle = SKAction()
+    
+    func wiggleBalloon()
+    {
+        for name in ["balloon_right", "balloon_left"] {
+            balloonAnimation.append(SKTexture(imageNamed: name))
+        }
+        
+        animateBalloonWiggle = SKAction.animate(
+            with: balloonAnimation,
+            timePerFrame: 0.3,
+            resize: true,
+            restore: false
+        )
+        
+        self.run(SKAction.repeatForever(animateBalloonWiggle))
+    }
+} 
