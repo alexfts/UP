25211172321c42e49c294aea55e1d2c24a3516f1
Reza Asad <reza@Rezas-MacBook-Pro.local>
2017-07-02 13:37:11 -0700

    Balloon moving according to physics laws

--- Up/GameScene.swift
+++ Up/GameScene.swift
@@ -9,6 +9,39 @@
 import SpriteKit
 import GameplayKit
 
+func + (left: CGPoint, right: CGPoint) -> CGPoint
+{
+    return CGPoint(x: left.x + right.x, y: left.y + right.y)
+}
+
+func - (left: CGPoint, right: CGPoint) -> CGPoint
+{
+    return CGPoint(x: left.x - right.x, y: left.y - right.y)
+}
+
+func * (point: CGPoint, scalar: CGFloat) -> CGPoint
+{
+    return CGPoint(x: point.x * scalar, y: point.y * scalar)
+}
+
+func / (point: CGPoint, scalar: CGFloat) -> CGPoint
+{
+    return CGPoint(x: point.x / scalar, y: point.y / scalar)
+}
+
+extension CGPoint
+{
+    func length() -> CGFloat
+    {
+        return sqrt(x*x+y*y)
+    }
+    
+    func normalize() -> CGPoint
+    {
+        return self / length()
+    }
+}
+
 class GameScene: SKScene {
     
     private var bg1: BGClass?
@@ -16,12 +49,30 @@ class GameScene: SKScene {
     private var bg3: BGClass?
     private var mainCamera: SKCameraNode?
     private var balloon: BalloonClass?
+    private var initialTouchTimestamp: TimeInterval?
     
     let UPWARD_SPEED: CGFloat = 5
     
     override func didMove(to view: SKView)
     {
         initializeGame()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else {
+            return
+        }
+        initialTouchTimestamp = touch.timestamp
+    }
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else {
+            return
+        }
+        let touchLocation = touch.location(in: self)
+        if let initialTouchTimestamp = initialTouchTimestamp {
+            let touchDuration = touch.timestamp - initialTouchTimestamp
+            balloon!.moveBalloon(touchLocation: touchLocation, touchDuration: touchDuration)
+        }
     }
     
     override func update(_ currentTime: TimeInterval)
@@ -38,6 +89,7 @@ class GameScene: SKScene {
         bg2 = childNode(withName: "BG2") as? BGClass!
         bg3 = childNode(withName: "BG3") as? BGClass!
         balloon = childNode(withName: "balloon") as? BalloonClass
+        balloon!.initializePhysicsWorld()
         balloon!.wiggleBalloon()
         
     }
